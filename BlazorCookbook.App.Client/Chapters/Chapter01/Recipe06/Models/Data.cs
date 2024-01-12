c4f0e499ba4d9cf530da82915dadb093f526a438
plbazyluk <plbazyluk@gmail.com>
2024-01-12 15:44:37 +0100

    features(ch1r06, ch1r07)

---
+++ BlazorCookbook.App.Client/Chapters/Chapter01/Recipe06/Models/Data.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter01.Recipe06.Models;
+
+public static class Data
+{
+    public static readonly TicketViewModel[] Tickets = [
+        new("Adult", 10.00m),
+        new("Free Admission", 0.00m),
+        new("Elderly", 8.00m),
+        new("Child", 5.00m),
+        new("Student", 5.00m),
+        new("Family", 20.00m),
+        new("Class", 100.00m)
+    ];
+}
---
+++ BlazorCookbook.App.Client/Chapters/Chapter01/Recipe07/Models/Data.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter01.Recipe07.Models;
+
+public static class Data
+{
+    public static readonly TicketViewModel[] Tickets = [
+        new("Adult", 10.00m, 100),
+        new("Free Admission", 0.00m, 100),
+        new("Elderly", 8.00m, 0),
+        new("Child", 5.00m, 0),
+        new("Student", 5.00m, 0),
+        new("Family", 20.00m, 0),
+        new("Class", 100.00m, 0)
+    ];
+}
