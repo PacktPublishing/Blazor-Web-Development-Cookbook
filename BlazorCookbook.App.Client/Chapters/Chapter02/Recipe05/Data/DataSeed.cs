5062f67e9298b2336bd8360f5f1c742a46dca113
plbazyluk <plbazyluk@gmail.com>
2024-01-30 16:20:26 +0100

    feature(ch02r05): implemented

---
+++ BlazorCookbook.App.Client/Chapters/Chapter02/Recipe05/Data/DataSeed.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter02.Recipe05.Data;
+
+public static class DataSeed
+{
+    public static readonly IList<SkillLevel> SkillLevels = [
+        new(1, "Basic"),
+        new(2, "Intermediate"),
+        new(3, "Advanced"),
+        new(4, "Expert")
+    ];
+}
---
+++ BlazorCookbook.App.Client/Chapters/Chapter02/Recipe05/Data/SkillLevel.cs
@@ -0,0 +1,3 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter02.Recipe05.Data;
+
+public record SkillLevel(int Id, string Title);
