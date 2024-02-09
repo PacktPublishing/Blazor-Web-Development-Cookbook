7f8753a5ffdbfd6f92ed6eefdc6a5e518466c9c0
plbazyluk <plbazyluk@gmail.com>
2024-02-09 16:18:14 +0100

    feature(ch03r01): implemented

---
+++ BlazorCookbook.App.Client/Chapters/Chapter03/Data/Ticket.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter03.Data;
+
+internal class Ticket
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+
+    public Ticket(string title)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+    }
+}
---
+++ BlazorCookbook.App.Client/Chapters/Chapter03/Data/Tickets.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter03.Data;
+
+internal static class Tickets
+{
+    public static Ticket[] All = [
+        new("Adult"),
+        new("Child"),
+        new("Infant"),
+        new("Veteran"),
+        new("Elderly"),
+        new("Guardian"),
+        new("Disabled"),
+        new("Student"),
+        new("Honorary")
+    ];
+}
