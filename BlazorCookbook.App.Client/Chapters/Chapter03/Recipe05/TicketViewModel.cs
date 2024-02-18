69c7257343f4c1351216b75265ab94946986b25c
plbazyluk <plbazyluk@gmail.com>
2024-02-18 12:26:11 +0100

    feature(ch03r05): implemented

---
+++ BlazorCookbook.App.Client/Chapters/Chapter03/Recipe05/TicketViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCookbook.App.Client.Chapters.Chapter03.Data;
+
+namespace BlazorCookbook.App.Client.Chapters.Chapter03.Recipe05;
+
+internal sealed class TicketViewModel(Ticket model)
+{
+    public Ticket Model { get; init; } = model;
+
+    private bool _isExpanded;
+    public void Expand() => _isExpanded = true;
+    public void Collapse() => _isExpanded = false;
+
+    public string DetailsStyle
+        => $"display: {(_isExpanded ? "block" : "none")};";
+}
