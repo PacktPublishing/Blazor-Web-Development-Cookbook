115fe6553facf92f139fdc17d3eff188913bd7a1
plbazyluk <plbazyluk@gmail.com>
2024-02-26 17:14:24 +0100

    feature(ch04e01): implemented

---
+++ BlazorCookbook.App.Client/Chapters/Chapter04/Data/Ticket.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter04.Data;
+
+public sealed record Ticket
+{
+    public Guid Id { get; init; }
+
+    public string Tariff { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Availability { get; set; }
+
+    public bool Enabled { get; set; } = true;
+
+    public Ticket()
+    {
+        Tariff = string.Empty;
+    }
+
+    public Ticket(string title, decimal price)
+    {
+        Id = Guid.NewGuid();
+        Tariff = title;
+        Price = price;
+    }
+}
---
+++ BlazorCookbook.App.Client/Chapters/Chapter04/Data/Tickets.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter04.Data;
+
+internal static class Tickets
+{
+    public static Ticket[] All = [
+        new("Adult", 20.00m),
+        new("Child", 10.00m),
+        new("Infant", 5.00m),
+        new("Veteran", 5.00m),
+        new("Elderly", 5.00m),
+        new("Guardian", 0.00m),
+        new("Disabled", 5.00m),
+        new("Student", 10.00m),
+        new("Honorary", 0.00m)
+    ];
+}
