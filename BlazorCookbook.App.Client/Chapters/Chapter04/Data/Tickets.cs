184f24536cd96a639c9b81ec672575e94e652958
plbazyluk <plbazyluk@gmail.com>
2024-03-09 11:38:20 +0100

    feature(ch04r03): implemented

--- BlazorCookbook.App.Client/Chapters/Chapter04/Data/Tickets.cs
+++ BlazorCookbook.App.Client/Chapters/Chapter04/Data/Tickets.cs
@@ -2,6 +2,18 @@
 
 internal static class Tickets
 {
+    private static string[] _tariffs = [
+        "Adult",
+        "Child",
+        "Infant",
+        "Veteran",
+        "Elderly",
+        "Guardian",
+        "Disabled",
+        "Student",
+        "Honorary"
+    ];
+
     public static Ticket[] All = [
         new("Adult", 20.00m),
         new("Child", 10.00m),
@@ -13,4 +25,24 @@ internal static class Tickets
         new("Student", 10.00m),
         new("Honorary", 0.00m)
     ];
+
+    public static Ticket[] LargeDataset
+    {
+        get
+        {
+            var randomizer = new Random();
+            var tickets = new List<Ticket>();
+            for (int i = 0; i < 500; i++)
+            {
+                var index = randomizer.Next(0, _tariffs.Length);
+                var price = Math.Round((decimal)randomizer.NextDouble() * 100, 2);
+                tickets.Add(new(_tariffs[index], price));
+            }
+            return [.. tickets];
+        }
+    }
+        
+        
+    
+
 }
