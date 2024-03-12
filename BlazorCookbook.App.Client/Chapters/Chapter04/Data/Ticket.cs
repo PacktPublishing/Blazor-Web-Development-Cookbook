335fefc198ef50a3c732ca8fdfe3262c9d734a73
plbazyluk <plbazyluk@gmail.com>
2024-03-12 17:05:58 +0100

    fix(ch04): removed Availability property of Ticket to reduce code complexity

--- BlazorCookbook.App.Client/Chapters/Chapter04/Data/Ticket.cs
+++ BlazorCookbook.App.Client/Chapters/Chapter04/Data/Ticket.cs
@@ -8,15 +8,6 @@ public sealed record Ticket
 
     public decimal Price { get; set; }
 
-    public int Availability { get; set; }
-
-    public bool Enabled { get; set; } = true;
-
-    public Ticket()
-    {
-        Tariff = string.Empty;
-    }
-
     public Ticket(string title, decimal price)
     {
         Id = Guid.NewGuid();
