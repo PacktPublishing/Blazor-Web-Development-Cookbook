164154441a1da7167228ec42372e8da5292bb56f
plbazyluk <plbazyluk@gmail.com>
2024-04-02 16:53:44 +0200

    feature(nav): moved chapters to nav menu

---
+++ BlazorCookbook.App/Configuration/ChaptersContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorCookbook.App.Configuration;
+
+internal static class ChaptersContainer
+{
+    private static readonly Dictionary<int, int> _chapters = new()
+    {
+        { 1, 7 },
+        { 2, 6 },
+        { 3, 7 },
+        { 4, 7 },
+        { 5, 8 },
+        { 6, 0 },
+        { 7, 0 },
+        { 8, 0 },
+        { 9, 0 },
+        { 10, 0 }
+    };
+
+    public static int HowManyChapters => _chapters.Count;
+
+    public static int HowManyRecipes(int number) => _chapters[number];
+}
