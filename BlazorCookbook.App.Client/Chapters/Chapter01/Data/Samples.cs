978b638e6fb34b4adab721037ba1058eadc3bc42
plbazyluk <plbazyluk@gmail.com>
2024-04-12 15:13:07 +0200

    fix(ch01r06): adjusted to content after tech-revs

--- BlazorCookbook.App.Client/Chapters/Chapter01/Data/Samples.cs
+++ BlazorCookbook.App.Client/Chapters/Chapter01/Data/Samples.cs
@@ -6,4 +6,14 @@ public static class Samples
         Adult = new("Adult", 10.00m),
         FreeAdmission = new("Free Admission", 0.00m),
         Elderly = new("Elderly", 8.00m);
+
+    public static readonly TicketViewModel[] Tickets = [
+        new("Adult", 10.00m),
+        new("Free Admission", 0.00m),
+        new("Elderly", 8.00m),
+        new("Child", 5.00m),
+        new("Student", 5.00m),
+        new("Family", 20.00m),
+        new("Class", 100.00m)
+    ];
 }
