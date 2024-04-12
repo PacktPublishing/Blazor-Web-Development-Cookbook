5053244f66ee66fe54bb67f3269690dab83324e1
plbazyluk <plbazyluk@gmail.com>
2024-04-12 15:33:09 +0200

    fix(ch01r07): adjusted to content after tech-revs

--- BlazorCookbook.App.Client/Chapters/Chapter01/Data/Samples.cs
+++ BlazorCookbook.App.Client/Chapters/Chapter01/Data/Samples.cs
@@ -3,17 +3,17 @@
 public static class Samples
 {
     public static readonly TicketViewModel
-        Adult = new("Adult", 10.00m),
-        FreeAdmission = new("Free Admission", 0.00m),
-        Elderly = new("Elderly", 8.00m);
+        Adult = new("Adult", 10.00m, 100),
+        FreeAdmission = new("Free Admission", 0.00m, 100),
+        Elderly = new("Elderly", 8.00m, 10);
 
     public static readonly TicketViewModel[] Tickets = [
-        new("Adult", 10.00m),
-        new("Free Admission", 0.00m),
-        new("Elderly", 8.00m),
-        new("Child", 5.00m),
-        new("Student", 5.00m),
-        new("Family", 20.00m),
-        new("Class", 100.00m)
+        new("Adult", 10.00m, 100),
+        new("Free Admission", 0.00m, 100),
+        new("Elderly", 8.00m, 0),
+        new("Child", 5.00m, 0),
+        new("Student", 5.00m, 0),
+        new("Family", 20.00m, 0),
+        new("Class", 100.00m, 0)
     ];
 }
--- BlazorCookbook.App.Client/Chapters/Chapter01/Data/TicketViewModel.cs
+++ BlazorCookbook.App.Client/Chapters/Chapter01/Data/TicketViewModel.cs
@@ -8,10 +8,13 @@ public record TicketViewModel
 
     public string Price { get; init; }
 
-    public TicketViewModel(string tariff, decimal price)
+    public int AvailableSeats { get; init; }
+
+    public TicketViewModel(string tariff, decimal price, int availableSeats)
     {
         Id = Guid.NewGuid();
         Tariff = tariff;
+        AvailableSeats = availableSeats;
 
         Price = price > 0 ?
                 price.ToString("0.00 $")
