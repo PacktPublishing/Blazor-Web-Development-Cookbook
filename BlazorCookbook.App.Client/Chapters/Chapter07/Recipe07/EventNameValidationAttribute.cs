182402d696611f22f1ca4c14e8a1bee9f0c9c83f
plbazyluk <plbazyluk@gmail.com>
2024-05-09 17:37:19 +0200

    fix(ch07r07): moved from r06, feature(ch07r06): implemented

---
+++ BlazorCookbook.App.Client/Chapters/Chapter07/Recipe07/EventNameValidationAttribute.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorCookbook.App.Client.Chapters.Chapter07.Recipe07;
+
+using System.ComponentModel.DataAnnotations;
+public class EventNameValidationAttribute : ValidationAttribute
+{
+    private const string _forbidden = "event";
+
+    private static ValidationResult Failure(string message, string member)
+        => new(message, [member]);
+
+    protected override ValidationResult IsValid(object value,
+        ValidationContext validationContext)
+    {
+        var text = value?.ToString();
+
+        if (string.IsNullOrWhiteSpace(text))
+            return Failure("You must provide name.",
+                validationContext.MemberName);
+
+        if (text.Contains(_forbidden, StringComparison.InvariantCultureIgnoreCase))
+            return Failure("You musn't use the 'event' keyword.",
+                validationContext.MemberName);
+
+        return ValidationResult.Success;
+    }
+}
