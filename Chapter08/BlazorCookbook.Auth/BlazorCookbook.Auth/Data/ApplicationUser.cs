a0402fcddf5152c4b6b554a5c23d5f5c65d52249
plbazyluk <plbazyluk@gmail.com>
2024-05-28 18:41:59 +0200

    feature(ch08r06): implemented

--- Chapter08/BlazorCookbook.Auth/BlazorCookbook.Auth/Data/ApplicationUser.cs
+++ Chapter08/BlazorCookbook.Auth/BlazorCookbook.Auth/Data/ApplicationUser.cs
@@ -2,9 +2,9 @@ using Microsoft.AspNetCore.Identity;
 
 namespace BlazorCookbook.Auth.Data
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
-
 }
--- Chapter08/BlazorCookbook.Auth/BlazorCookbook.Auth/Data/Migrations/20240527151443_AddedUserFullName.Designer.cs
+++ Chapter08/BlazorCookbook.Auth/BlazorCookbook.Auth/Data/Migrations/20240527151443_AddedUserFullName.Designer.cs
@@ -4,6 +4,7 @@ using BlazorCookbook.Auth.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorCookbook.Auth.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240527151443_AddedUserFullName")]
+    partial class AddedUserFullName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
