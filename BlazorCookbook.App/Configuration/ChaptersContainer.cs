bd1e8341e1623bac4ab6c6fbc2867f95d4ed58cf
plbazyluk <plbazyluk@gmail.com>
2024-06-19 16:32:46 +0200

    fix(toc): adjusted how toc is generated

--- BlazorCookbook.App/Configuration/ChaptersContainer.cs
+++ BlazorCookbook.App/Configuration/ChaptersContainer.cs
@@ -2,20 +2,16 @@
 
 internal static class ChaptersContainer
 {
-    private static readonly Dictionary<int, int> _chapters = new()
-    {
-        { 1, 7 },
-        { 2, 6 },
-        { 3, 7 },
-        { 4, 7 },
-        { 5, 7 },
-        { 6, 6 },
-        { 7, 7 },
-        { 9, 9 },
-        { 10, 0 }
-    };
-
-    public static int HowManyChapters => _chapters.Count;
-
-    public static int HowManyRecipes(int number) => _chapters[number];
+    public static readonly List<(int No, int Recipes)> Values =
+    [
+        ( 1, 7 ),
+        ( 2, 6 ),
+        ( 3, 7 ),
+        ( 4, 7 ),
+        ( 5, 7 ),
+        ( 6, 6 ),
+        ( 7, 7 ),
+        ( 9, 9 ),
+        ( 10, 0 )
+    ];
 }
